9249e3c86d4032dc6f42752e10275856cffe9ed3
Mohit-codex <kumarmohit9343@gmail.com>
2026-05-06 17:41:24 +0530

    program to reverse string

--- Dayone.java
+++ Dayone.java
@@ -1,3 +1,11 @@
+
 public class Dayone {
-    
+    public static void main(String[] args) {
+        String[] str ={"R","A","J","A","S","T","H","A","N"};
+        System.out.println("the given string is : RAJASTHAN");
+        System.out.print("the reverse string will be:");
+    for(int i=0;i<str.length;i++){
+        System.out.print(str[str.length-1-i]);
+    }
+    }
 }
